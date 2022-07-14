93a1d95633ff73765a31a0c8f14d13fbd40b3372
Alex <Koblosh.Alex@gmail.com>
2022-07-13 22:32:52 -0400

    progress

--- hashTable/HashTable.js
+++ hashTable/HashTable.js
@@ -1,12 +1,12 @@
 class HashTable {
-  constructor(size){
+  constructor(size) {
     this.data = new Array(size);
   }
 
   _hash(key) {
     let hash = 0;
-    for (let i =0; i < key.length; i++){
-      hash = (hash + key.charCodeAt(i) * i) % this.data.length
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
     }
     return hash;
   }
@@ -20,22 +20,32 @@ class HashTable {
     return this.data;
   }
 
-  get(key){
+  get(key) {
     const address = this._hash(key);
-    const currentBucket = this.data[address]
+    const currentBucket = this.data[address];
     if (currentBucket) {
-      for(let i = 0; i < currentBucket.length; i++){
-        if(currentBucket[i][0] === key) {
-          return currentBucket[i][1]
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
         }
       }
     }
     return undefined;
   }
+
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+    return keysArray;
+  }
 }
 
 const myHashTable = new HashTable(50);
-myHashTable.set('grapes', 10000)
-myHashTable.get('grapes')
-myHashTable.set('apples', 9)
-console.log(myHashTable.get('apples'))
+myHashTable.set("grapes", 10000);
+myHashTable.get("grapes");
+myHashTable.set("apples", 9);
+console.log(myHashTable.keys());
