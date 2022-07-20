22655aa04f054e04adc2fd878a554ab4d23420eb
Alex <Koblosh.Alex@gmail.com>
2022-07-20 17:22:16 -0400

    progress

---
+++ stack/Stack.js
@@ -0,0 +1,54 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.top;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    newNode.next = this.top;
+    this.top = newNode;
+    this.bottom = newNode;
+    this.length++;
+  }
+  // 3  <- top
+  // 2
+  // 1
+  pop() {
+    if (!this.top) return null;
+
+    if (this.top === this.bottom) this.bottom = null;
+
+    this.top = this.top.next;
+    this.length--;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
+const stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.pop();
+stack.pop();
+stack.pop();
+console.log(stack.bottom);
+
+console.log(stack.peek());
+console.log(stack.isEmpty());
